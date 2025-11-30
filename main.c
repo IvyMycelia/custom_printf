f68c5a42b2fff9c37f8807ff21edbac20afff3d0
Ivy Banana <cnnorwood14@gmail.com>
2025-11-30 03:54:06 -0500

    Changed ASM
    Instead of inline ASM, implemented separate wrappers. This will keep the actual function a lot nicer to look at.
    
    Next: Still need to figure out (and fix) the non-exiting issue..

--- main.c
+++ main.c
@@ -2,12 +2,5 @@
 
 int main(int argc, char *argv[]) {
     if (argc < 2) return 1;
-    return print(argv[1]);
+    print(argv[1]);
 }
-
-/*
- * RETURN CODES:
- *  -1 —> INVALID ARCHITECTURE
- *  0  —> SUCCESS
- *  1  —> NO PRINT INPUT
-*/
--- print.c
+++ print.c
@@ -2,6 +2,56 @@
 // Created by Ivy on 11/29/25.
 //
 
+#if defined(__x86_64__)
+    #define SYS_WRITE()                     \
+        asm volatile (                      \
+            "movq $0x2000004, %%rax\n"      \
+            "movq $1, %%rdi\n"              \
+            "movq %0, %%rsi\n"              \
+            "movq %1, %%rdx\n"              \
+            "syscall\n"                     \
+            :                               \
+            : "r"(c), "r"(l)                \
+            : "%rax", "%rdi", "%rsi", "%rdx"\
+        );
+
+    #define SYS_EXIT()                      \
+        asm volatile (                      \
+            "movq $0x2000001, %%rax\n"      \
+            "movq $0, %%rdi\n"              \
+            "syscall\n"                     \
+            :                               \
+            : "r"(c), "r"(l)                \
+            : "%rax", "%rdi", "%rsi", "%rdx"\
+        );
+
+#elif defined(__aarch64__)
+    #define SYS_WRITE()                 \
+        asm volatile (                  \
+            "mov x0, #1\n"              \
+            "mov x1, %0\n"              \
+            "mov x2, %1\n"              \
+            "ldr x16, =0x2000004\n"     \
+            "svc #0\n"                  \
+            :                           \
+            : "r"(c), "r"(l)            \
+            : "x0", "x1", "x2", "x16"   \
+        );
+
+    #define SYS_EXIT()              \
+        asm volatile (              \
+            "mov x0, #0\n"          \
+            "ldr x16, =0x2000001\n" \
+            "svc #0"                \
+            :                       \
+            :                       \
+            : "x0", "x16"           \
+        );
+
+#else
+    #error "Unrecognized OS Architecture"
+#endif
+
 int print(char *c) {
     // Measure length
     unsigned long l = 0;
@@ -9,46 +59,10 @@ int print(char *c) {
     while (*p != '\0') l++;
 
     // Write
-    #if defined(__x86_64__)
-        // x86_64
-        asm volatile (
-                "movq $0x2000004, %%rax\n"  // syscall number for write
-                "movq $1, %%rdi\n"          // stdout
-                "movq %0, %%rsi\n"          // buffer (content)
-                "movq %1, %%rdx\n"          // length of content
-                "syscall\n"
+    SYS_WRITE();
 
-                "movq $0x2000001, %%rax\n"   // syscall number for exit
-                "movq $0, %%rdi\n"           // exit code '0'
-                "syscall\n"
-                :
-                : "r"(c), "r"(l)
-                : "%rax", "%rdi", "%rsi", "%rdx"
-                );
-    #elif defined(__aarch64__)
-        // ARM
-        asm volatile (
-                "mov x0, #1\n"          // stdout
-                "mov x1, %0\n"          // buffer (content)
-                "mov x2, %1\n"          // length of content
-                "ldr x16, =0x2000004\n" // syscall number for write
-                "svc #0\n"
-                :
-                : "r"(c), "r"(l)
-                : "x0", "x1", "x2", "x16"
-        );
+    // Exit
+    SYS_EXIT();
 
-        // Exit
-        asm volatile (
-                "mov x0, #0\n"          // exit code '0'
-                "ldr x16, =0x2000001\n" // syscall number for exit
-                "svc #0"
-                :
-                :
-                : "x0", "x16"
-        );
-    #else
-    #error "Unrecognized OS Architecture"
-    #endif
     return 0;
 }
