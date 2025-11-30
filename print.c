a2166db3fe9c9d3348290faca6a32b4f1edb069e
Ivy Banana <cnnorwood14@gmail.com>
2025-11-30 05:14:52 -0500

    Changed `main.c` & `print.c`: - Moved logic from `main.c` —> `print.c`:  - `print.c` retains full functionality  - `main.c` remains as just an example on using `print()` - Cleaned up logic

--- print.c
+++ print.c
@@ -2,12 +2,8 @@
 // Created by Ivy on 11/29/25.
 //
 
-void print(char *c) {
-    unsigned long l = 0;
-    char *p;
-    for (p = c; *p; p++) l++;
-
-    #if defined(__x86_64__)
+static void print_char(char c) {
+#if defined(__x86_64__)
     asm volatile (
         "movq $0x2000004, %%rax\n"
         "movq $1, %%rdi\n"
@@ -15,16 +11,39 @@ void print(char *c) {
         "movq %1, %%rdx\n"
         "syscall\n"
         :
-        : "r"(c), "r"(l)
+        : "r"(&c)
         : "%rax", "%rdi", "%rsi", "%rdx"
     );
-    #elif defined(__aarch64__)
-        register const char *x1 asm("x1") = c;
-        register unsigned long x2 asm("x2") = l;
-        register unsigned long x0 asm("x0") = 1;
-        register unsigned long x16 asm("x16") = 0x2000004;
-        asm volatile("svc #0" : : "r"(x0), "r"(x1), "r"(x2), "r"(x16) : "memory");
-    #else
-        #error "Unsupported architecture"
-    #endif
+#elif defined(__aarch64__)
+    register const char *x1 asm("x1") = &c;
+    register unsigned long x2 asm("x2") = 1;
+    register unsigned long x0 asm("x0") = 1;
+    register unsigned long x16 asm("x16") = 0x2000004;
+    asm volatile("svc #0" : : "r"(x0), "r"(x1), "r"(x2), "r"(x16) : "memory");
+#else
+#error "Unsupported architecture"
+#endif
+}
+
+void print(char *c) {
+    char *p = c;
+    while (*p) {
+        if (*p == '\\') {
+            p++;
+            if (*p == '\0') break;
+            switch (*p) {
+                case 'n':   print_char('\n');   break;
+                case 't':   print_char('\t');   break;
+                case '\\':  print_char('\\');   break;
+                case '"':   print_char('"');    break;
+                case 'r':   print_char('\r');   break;
+                case '0':   print_char('\0');   break;
+                default:    print_char(*p);     break;
+            }
+            p++;
+        } else {
+            print_char(*p);
+            p++;
+        }
+    }
 }
