c8bf774faae383df1c31e322b406fcdca1e4b0e5
Ivy Banana <cnnorwood14@gmail.com>
2025-12-01 16:30:39 -0500

    Updated library

--- print/src/print.c
+++ print/src/print.c
@@ -2,48 +2,73 @@
 // Created by Ivy on 11/29/25.
 //
 
-static void print_char(char c) {
-#if defined(__x86_64__)
+// print.c
+#include "../include/print.h"
+
+static inline void sys_write_char(char c) {
+#if defined(__x86_64__) && defined(__APPLE__)
+    // macOS x86_64
     asm volatile (
         "movq $0x2000004, %%rax\n"
         "movq $1, %%rdi\n"
         "movq %0, %%rsi\n"
-        "movq %1, %%rdx\n"
+        "movq $1, %%rdx\n"
         "syscall\n"
         :
         : "r"(&c)
         : "%rax", "%rdi", "%rsi", "%rdx"
     );
-#elif defined(__aarch64__)
+#elif defined(__x86_64__) && defined(__linux__)
+    // Linux x86_64
+    asm volatile (
+        "movq $1, %%rax\n"
+        "movq $1, %%rdi\n"
+        "movq %0, %%rsi\n"
+        "movq $1, %%rdx\n"
+        "syscall\n"
+        :
+        : "r"(&c)
+        : "%rax", "%rdi", "%rsi", "%rdx"
+    );
+#elif defined(__aarch64__) && defined(__APPLE__)
+    // macOS ARM64
     register const char *x1 asm("x1") = &c;
-    register unsigned long x2 asm("x2") = 1;
     register unsigned long x0 asm("x0") = 1;
+    register unsigned long x2 asm("x2") = 1;
     register unsigned long x16 asm("x16") = 0x2000004;
     asm volatile("svc #0" : : "r"(x0), "r"(x1), "r"(x2), "r"(x16) : "memory");
+#elif defined(__aarch64__) && defined(__linux__)
+    // Linux ARM64
+    register const char *x1 asm("x1") = &c;
+    register unsigned long x0 asm("x0") = 1;
+    register unsigned long x2 asm("x2") = 1;
+    register unsigned long x8 asm("x8") = 64; // sys_write in Linux ARM64
+    asm volatile("svc #0" : : "r"(x0), "r"(x1), "r"(x2), "r"(x8) : "memory");
 #else
-#error "Unsupported architecture"
+    #error "Unsupported architecture/OS"
 #endif
 }
 
-void print(char *c) {
-    char *p = c;
-    while (*p) {
-        if (*p == '\\') {
-            p++;
-            if (*p == '\0') break;
-            switch (*p) {
-                case 'n':   print_char('\n');   break;
-                case 't':   print_char('\t');   break;
-                case '\\':  print_char('\\');   break;
-                case '"':   print_char('"');    break;
-                case 'r':   print_char('\r');   break;
-                case '0':   print_char('\0');   break;
-                default:    print_char(*p);     break;
+void print_char(char c) {
+    sys_write_char(c);
+}
+
+void print(const char *c) {
+    while (*c) {
+        if (*c == '\\') {
+            c++;
+            if (*c == '\0') break;
+            switch (*c) {
+                case 'n': print_char('\n'); break;
+                case 't': print_char('\t'); break;
+                case '\\': print_char('\\'); break;
+                case '"': print_char('"'); break;
+                case 'r': print_char('\r'); break;
+                default: print_char(*c); break;
             }
-            p++;
+            c++;
         } else {
-            print_char(*p);
-            p++;
+            print_char(*c++);
         }
     }
 }
